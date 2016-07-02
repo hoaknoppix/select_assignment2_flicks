07b6e04c8039a6143f3858f004f951f6dead70ae
Hoa Tran <hoaknoppix@gmail.com>
2016-07-02 14:28:25 +0700

    First commit: Initializing projects

---
+++ select_assignment2_flicks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  select_assignment2_flicks
+//
+//  Created by hoaqt on 7/2/16.
+//  Copyright © 2016 com.noron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
