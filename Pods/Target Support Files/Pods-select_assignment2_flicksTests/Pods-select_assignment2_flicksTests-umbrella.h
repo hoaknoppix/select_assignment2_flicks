e2a7f784245318b020136352f2eefea3cd32543d
Hòa Trần <hoaknoppix@gmail.com>
2016-07-03 23:55:25 +0700

    Revert "Second commit: Adding basic functions"

--- Pods/Target Support Files/Pods-select_assignment2_flicksTests/Pods-select_assignment2_flicksTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_select_assignment2_flicksTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_select_assignment2_flicksTestsVersionString[];
-
